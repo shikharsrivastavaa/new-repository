7f687a0e087fbb5eec7541b69d9128164b97dcd6
shikhasr <shikhar.srivastava@amdocs.com>
2024-06-29 13:32:00 +0530

    Adding some of two numbers

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world");
+        System.out.println(sumOfTwoNumbers(1,3));
         System.out.println("Difference of two Numbers: "+ diffOfTwoNumbers(2,3));
     }
 
     public static int diffOfTwoNumbers(int a,int b){
         return Math.abs(a-b);
     }
+
+    public static int sumOfTwoNumbers(int a, int b){
+        return a+b;
+    }
+
 }
