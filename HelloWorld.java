a8e92c8b8b7ba2c61589c9cab5a4efe68a752ae9
shikhasr <shikhar.srivastava@amdocs.com>
2024-06-29 13:37:16 +0530

    Difference of two numbers

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
         System.out.println("Hello world");
         System.out.println(sumOfTwoNumbers(1,3));
         System.out.println("Difference of two Numbers: "+ diffOfTwoNumbers(2,3));
+        System.out.println("Product of two numbers: "+productOfTwoNumber(2,3));
     }
 
     public static int diffOfTwoNumbers(int a,int b){
@@ -11,6 +12,10 @@ public class HelloWorld {
 
     public static int sumOfTwoNumbers(int a, int b){
         return a+b;
+    }
+
+    public static int productOfTwoNumber(int a,int b){
+        return a*b;
     }
 
 }
