1c4619db106e0f1b928b36bc8860ead076e621fe
shikhasr <shikhar.srivastava@amdocs.com>
2024-06-29 13:33:49 +0530

    Difference of two numbers

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world");
+        System.out.println("Difference of two Numbers: "+ diffOfTwoNumbers(2,3));
+    }
+
+    public static int diffOfTwoNumbers(int a,int b){
+        return Math.abs(a-b);
     }
 }
